7dcbdea40446749e30e8a84452b541047a6e4b79
nhaar <nhaar224131@gmail.com>
2023-11-29 08:57:36 -0300

    add ample version architecture/support

--- src/converter.py
+++ src/converter.py
@@ -4,7 +4,19 @@ import shutil
 import subprocess
 import argparse
 
-parser = argparse.ArgumentParser(description='Converts the game to a Linux-compatible version.')
+GAME_VERSIONS = []
+
+# Using plural or not
+version_noun = 'versions' if len(GAME_VERSIONS) > 1 else 'version'
+
+# Listing in English
+version_string = ''
+if len(GAME_VERSIONS) == 1:
+    version_string = GAME_VERSIONS[0]
+else:
+    version_string = f"{', '.join(GAME_VERSIONS[:-1])}, and {GAME_VERSIONS[-1]}"
+
+parser = argparse.ArgumentParser(description=f'Converts the game to a Linux-compatible version. Works for Deltarune Steam {version_noun} {version_string}.')
 
 parser.add_argument('arg1', nargs='?', type=str, help='Path to the game directory, if not given it will default to the default Steam directory')
 parser.add_argument('arg2', nargs='?', type=str, help='Path to the new game directory, if not given it will be created in the script\'s directory')
@@ -25,7 +37,7 @@ if (not os.path.isdir(DELTARUNE_PATH)):
     if (args.arg1):
         print(f'The game directory {args.arg1} was not found.')
     else:
-        print('The game directory was not found. Please supply it as an argument.')
+        print('The game directory was not found. Please supply it as an argument or make sure the game is installed.')
     subprocess.run(['python3', os.path.realpath(__file__), '-h'])
     exit()
 
@@ -39,166 +51,7 @@ DELTARUNE_DIRECTORIES = [
 ]
 
 # File that will be copied, converting to lower case because of case-insensitive filesystems
-DELTARUNE_FILES = [
-    'audiogroup1.dat',
-    'AUDIO_INTRONOISE.ogg',
-    'AUDIO_INTRONOISE_ch1.ogg',
-    'data.win',
-    'options.ini',
-    'snd_bigcar_yelp.ogg',
-    'snd_closet_fall.ogg',
-    'snd_closet_fall_ch1.ogg',
-    'snd_closet_impact.ogg',
-    'snd_closet_impact_ch1.ogg',
-    'snd_dtrans_drone.ogg',
-    'snd_dtrans_flip.ogg',
-    'snd_dtrans_heavypassing.ogg',
-    'snd_dtrans_lw.ogg',
-    'snd_dtrans_square.ogg',
-    'snd_dtrans_twinkle.ogg',
-    'snd_fountain_make.ogg',
-    'snd_fountain_target.ogg',
-    'snd_ghostappear.ogg',
-    'snd_great_shine.ogg',
-    'snd_great_shine_ch1.ogg',
-    'snd_him_quick.ogg',
-    'snd_hitcar.ogg',
-    'snd_hitcar_little.ogg',
-    'snd_icespell.ogg',
-    'snd_paper_rumble.ogg',
-    'snd_paper_rumble_ch1.ogg',
-    'snd_paper_surf.ogg',
-    'snd_paper_surf_ch1.ogg',
-    'snd_power',
-    'snd_revival.ogg',
-    'snd_revival_ch1.ogg',
-    'snd_rurus_appear.ogg',
-    'snd_rurus_appear_ch1.ogg',
-    'snd_smallcar_yelp.ogg',
-    'snd_snowgrave.ogg',
-    'snd_spell_pacify.ogg',
-    'snd_usefountain.ogg',
-    'snd_usefountain_ch1.ogg',
-    'Steamworks.dll',
-    'lang/lang_en_ch1.json',
-    'lang/lang_ja.json',
-    'lang/lang_ja_ch1.json',
-    'mus/acid_tunnel.ogg',
-    'mus/alarm_titlescreen.ogg',
-    'mus/alley_ambience.ogg',
-    'mus/april_2012.ogg',
-    'mus/AUDIO_ANOTHERHIM.ogg',
-    'mus/AUDIO_DARKNESS.ogg',
-    'mus/AUDIO_DEFEAT.ogg',
-    'mus/AUDIO_DRONE.ogg',
-    'mus/AUDIO_STORY.ogg',
-    'mus/basement.ogg',
-    'mus/battle.ogg',
-    'mus/berdly_audience.ogg',
-    'mus/berdly_battle_heartbeat_true.ogg',
-    'mus/berdly_chase.ogg',
-    'mus/berdly_descend.ogg',
-    'mus/berdly_flashback.ogg',
-    'mus/berdly_theme.ogg',
-    'mus/bird.ogg',
-    'mus/boxing_boss.ogg',
-    'mus/boxing_game.ogg',
-    'mus/card_castle.ogg',
-    'mus/castletown.ogg',
-    'mus/castletown_empty.ogg',
-    'mus/ch2_credits.ogg',
-    'mus/charjoined.ogg',
-    'mus/checkers.ogg',
-    'mus/coolbeat.ogg',
-    'mus/creepychase.ogg',
-    'mus/creepydoor.ogg',
-    'mus/creepylandscape.ogg',
-    'mus/cyber.ogg',
-    'mus/cybercity.ogg',
-    'mus/cybercity_alt.ogg',
-    'mus/cybercity_old.ogg',
-    'mus/cyberhouse.ogg',
-    'mus/cybershop_christmas.ogg',
-    'mus/cyber_battle.ogg',
-    'mus/cyber_battle_end.ogg',
-    'mus/cyber_battle_prelude.ogg',
-    'mus/cyber_shop.ogg',
-    'mus/d.ogg',
-    'mus/deep_noise.ogg',
-    'mus/dogcheck.ogg',
-    'mus/dontforget.ogg',
-    'mus/elevator.ogg',
-    'mus/fanfare.ogg',
-    'mus/field_of_hopes.ogg',
-    'mus/flashback_excerpt.ogg',
-    'mus/forest.ogg',
-    'mus/friendship.ogg',
-    'mus/GALLERY.ogg',
-    'mus/gameover_short.ogg',
-    'mus/giant_queen_appears.ogg',
-    'mus/gigaqueen_pre.ogg',
-    'mus/hip_shop.ogg',
-    'mus/home.ogg',
-    'mus/honksong.ogg',
-    'mus/joker.ogg',
-    'mus/KEYGEN.ogg',
-    'mus/kingboss.ogg',
-    'mus/lancer.ogg',
-    'mus/lancerfight.ogg',
-    'mus/lancer_susie.ogg',
-    'mus/legend.ogg',
-    'mus/man.ogg',
-    'mus/mansion.ogg',
-    'mus/mansion_entrance.ogg',
-    'mus/menu.ogg',
-    'mus/muscle.ogg',
-    'mus/music_guys.ogg',
-    'mus/music_guys_intro.ogg',
-    'mus/mus_birdnoise.ogg',
-    'mus/mus_introcar.ogg',
-    'mus/mus_school.ogg',
-    'mus/napsta_alarm.ogg',
-    'mus/noelle.ogg',
-    'mus/noelle_ferriswheel.ogg',
-    'mus/noelle_normal.ogg',
-    'mus/noelle_school.ogg',
-    'mus/ocean.ogg',
-    'mus/prejoker.ogg',
-    'mus/queen.ogg',
-    'mus/queen_boss.ogg',
-    'mus/queen_car_radio.ogg',
-    'mus/queen_intro.ogg',
-    'mus/quiet_autumn.ogg',
-    'mus/rouxls_battle.ogg',
-    'mus/ruruskaado.ogg',
-    'mus/shinkansen.ogg',
-    'mus/shop1.ogg',
-    'mus/sink_noise.ogg',
-    'mus/spamton_basement.ogg',
-    'mus/spamton_battle.ogg',
-    'mus/spamton_happy.ogg',
-    'mus/spamton_house.ogg',
-    'mus/spamton_laugh_noise.ogg',
-    'mus/spamton_meeting.ogg',
-    'mus/spamton_meeting_intro.ogg',
-    'mus/spamton_neo_after.ogg',
-    'mus/spamton_neo_meeting.ogg',
-    'mus/spamton_neo_mix_ex_wip.ogg',
-    'mus/static_placeholder.ogg',
-    'mus/s_neo.ogg',
-    'mus/s_neo_clip.ogg',
-    'mus/tense.ogg',
-    'mus/the_dark_truth.ogg',
-    'mus/THE_HOLY.ogg',
-    'mus/thrashmachine.ogg',
-    'mus/thrash_rating.ogg',
-    'mus/town.ogg',
-    'mus/tv_noise.ogg',
-    'mus/vs_susie.ogg',
-    'mus/w.ogg',
-    'mus/wind.ogg',
-    'mus/wind_highplace.ogg'
-]
+DELTARUNE_FILES = []
 
 assets_path = os.path.join(NEW_DELTARUNE_PATH, 'assets')
 
@@ -209,6 +62,10 @@ for file in DELTARUNE_FILES:
     file_path = os.path.join(DELTARUNE_PATH, file)
     if (os.path.isfile(file_path)):
         shutil.copy(file_path, os.path.join(assets_path, file.lower()))
+    else:
+        print('Game file not found:', file_path)
+        print('Make sure you have the correct game version installed and that you have supplied the correct game directory and try again.')
+        exit()
 
 # Rename it so it works on Linux
 os.rename(os.path.join(assets_path, 'data.win'), os.path.join(assets_path, 'game.unx'))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,34 @@
 import base64
 import os
+from copy import deepcopy
 
 from python_minifier import minify
+from constants import BASE_FILES, VERSION_DETAILS
+
+def replace_variable_in_code(code, variable_name, default_value, value):
+    '''Replaces a variable in the code with a new value.'''
+    return code.replace(f'{variable_name} = {default_value}', f'{variable_name} = {value}')
+
+def replace_string_variable_in_code(code, variable_name, value):
+    '''Replaces a string variable in the code with a new value.'''
+    return replace_variable_in_code(code, variable_name, "''", f"'{value}'")
+
+def replace_list_variable_in_code(code, variable_name, value):
+    '''Replaces a list variable in the code with a new value.'''
+    return replace_variable_in_code(code, variable_name, '[]', value)
+
+def create_for_version(version, base_code):
+    os.makedirs('output', exist_ok=True)
+    version_list = version['versions']
+    converter_code = replace_list_variable_in_code(base_code, 'GAME_VERSIONS', version_list)
+    files = deepcopy(BASE_FILES)
+    if 'exclusive_files' in version:
+        files.extend(version['exclusive_files'])
+
+    converter_code = replace_variable_in_code(converter_code, 'DELTARUNE_FILES', '[]', files)
+
+    with open(f'output/converter-{version["name"]}.py', 'w') as output_file:
+        output_file.write(minify(converter_code))
 
 def main():
     '''Main function, creates the converter file for production from a runner.'''
@@ -16,18 +43,17 @@ def main():
         binary_file_data = binary_file.read()
         encoded_binary = base64.b64encode(binary_file_data).decode('utf-8')
 
-    variables_to_define = {
+    string_variables_to_define = {
         'RUNNER_BINARY_STRING': encoded_binary,
         'LIBRARY_LINK': 'http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb',
         'LIBRARY_FILE': 'libssl1.1_1.1.1f-1ubuntu2.20_amd64.deb'
     }
 
-    for variable_name, variable_value in variables_to_define.items():
-        converter_code = converter_code.replace(f'{variable_name} = \'\'', f'{variable_name} = \'{variable_value}\'')
+    for variable_name, variable_value in string_variables_to_define.items():
+        converter_code = replace_string_variable_in_code(converter_code, variable_name, variable_value)
 
-    os.makedirs('output', exist_ok=True)
-    with open('output/converter.py', 'w') as output_file:
-        output_file.write(minify(converter_code))
+    for version_detail in VERSION_DETAILS:
+        create_for_version(version_detail, converter_code)
 
 if __name__ == '__main__':
     main()
